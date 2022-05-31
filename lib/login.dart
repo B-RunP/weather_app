8d2481ea41ebb17af5287fc02030edab34d4e511
FlutLab.io <>
2022-05-31 08:34:23 +0000

    membuat logi

---
+++ lib/login.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Widget extends StatefulWidget {
+  const Widget({Key? key}) : super(key: key);
+  _WidgetState createState() => _WidgetState();
+}
+
+class _WidgetState extends State<Widget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
