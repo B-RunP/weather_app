bae5195548e5e3041d48e18c66114d18ad3a1e91
FlutLab.io <>
2022-06-07 05:28:27 +0000

    Autosave

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/screens/signin_screen.dart';
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({Key? key}) : super(key: key);
@@ -6,8 +7,122 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
+  TextEditingController userNameController = TextEditingController();
+  TextEditingController userEmailController = TextEditingController();
+  TextEditingController userPhoneController = TextEditingController();
+  TextEditingController userPasswordController = TextEditingController();
+  TextEditingController userConfirmPasswordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Sign Up'),
+        ),
+        body: SingleChildScrollView(
+            child: Column(children: [
+          SizedBox(height: 20),
+          Text("Let's Get Started", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+          Text(
+            'Creat an account to Q alure to get all Leatures',
+            style: TextStyle(color: Colors.black.withOpacity(0.5)),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+            child: TextFormField(
+                controller: userNameController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  prefixIcon: Icon(Icons.person_outline, size: 20),
+                  // labelText: "Username",
+                  hintText: "Masukan Username",
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+            child: TextFormField(
+                controller: userEmailController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  prefixIcon: Icon(Icons.email_outlined, size: 20),
+                  // labelText: "Username",
+                  hintText: "Email",
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+            child: TextFormField(
+                controller: userPhoneController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  prefixIcon: Icon(Icons.phone_android_outlined, size: 20),
+                  // labelText: "Username",
+                  hintText: "Phone",
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+            child: TextFormField(
+                controller: userPasswordController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  prefixIcon: Icon(Icons.lock_open_outlined, size: 20),
+                  // labelText: "Username",
+                  hintText: "Password",
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+            child: TextFormField(
+                controller: userConfirmPasswordController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  prefixIcon: Icon(Icons.lock, size: 20),
+                  // labelText: "Username",
+                  hintText: "Confirm Password",
+                )),
+          ),
+          SizedBox(height: 20),
+          SizedBox(
+            width: 250,
+            height: 50,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Fitur masih dalam pengembangan')));
+              },
+              child: Text('Create'),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Already have an account?'),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => SignInScreen()));
+                },
+                child: Text(
+                  "Login here",
+                ),
+              ),
+            ],
+          ),
+        ])));
   }
 }
