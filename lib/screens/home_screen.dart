d8385664db5eca3bf40727344762bd4f36550d1c
FlutLab.io <>
2022-06-07 05:40:12 +0000

    Autosave

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/screens/signin_screen.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,6 +10,39 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+      ),
+      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Center(
+          child: Text('Nama: Brian Purnama Putra', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+        ),
+        Center(
+          child: Text('NIM: 190103003', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+        ),
+        SizedBox(height: 20),
+        SizedBox(
+          width: 150,
+          height: 30,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: Colors.green,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+            onPressed: () {
+              var box = Hive.box('userBox').clear();
+
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (_) => SignInScreen()),
+              );
+            },
+            child: Text('Logout'),
+          ),
+        ),
+      ]),
+    );
   }
 }
