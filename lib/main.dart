dbcdd804c016e97f6dfcec0c9b8851a6589becff
FlutLab.io <>
2022-06-15 02:59:53 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,21 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:myapp/screens/signin_screen.dart';
+import 'splash_screen.dart';
+import 'home_screen.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Stateful Clicker Counter',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // Application theme data, you can set the colors for the application as
-        // you want
         primarySwatch: Colors.blue,
       ),
-      home: const SignInScreen(),
+      home: splashScreen(),
     );
   }
 }
