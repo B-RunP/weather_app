ac34ae2763d14b0afeb9c47ec6372304068efdf9
FlutLab.io <>
2022-06-15 04:36:31 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/screens/signin_screen.dart';
+import 'package:myapp/screens/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
