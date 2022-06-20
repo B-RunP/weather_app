1b87342fbb3346e710ee007ae67e893f183f437a
FlutLab.io <>
2022-06-20 03:56:06 +0000

    Autosave

---
+++ lib/models/models.dart
@@ -0,0 +1,24 @@
+/*
+{
+  "weather": [
+    {
+      "id": 800,
+      "main": "Clear",
+      "description": "clear sky",
+      "icon": "01d"
+    }
+  ],
+  "main": {
+    "temp": 282.55,
+    "feels_like": 281.86,
+    "temp_min": 280.37,
+    "temp_max": 284.26,
+    "pressure": 1023,
+    "humidity": 100
+  },
+  "name": "Mountain View",
+  "cod": 200
+  }                         
+
+                        
+*/
