9d299af15307214229e2031aa40efd52332cf167
FlutLab.io <>
2022-06-21 01:09:58 +0000

    Autosave

---
+++ lib/screens/news_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NewsScreen extends StatefulWidget {
+  _NewsScreenState createState() => _NewsScreenState();
+}
+
+class _NewsScreenState extends State<NewsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Row(children: <Widget>[
+      Text("Flutter"),
+      Text("News", style: TextStyle(color: Colors.blue))
+    ])));
+  }
+}
