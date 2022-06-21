44927a2e3f10013e13b907c537b8c3c0c0ca6ff0
FlutLab.io <>
2022-06-21 06:36:57 +0000

    Autosave

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:myapp/screens/signup_screen.dart';
 import 'package:myapp/screens/weather_screen.dart';
+import 'package:myapp/screens/news_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -50,6 +51,22 @@ class HomeScreen extends StatelessWidget {
               Navigator.push(context, MaterialPageRoute(builder: (context) => WeatherScreen()));
             },
             child: Text('Cuaca'),
+          ),
+        ),
+        SizedBox(
+          width: 150,
+          height: 30,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: Colors.green,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => NewsScreen()));
+            },
+            child: Text('News'),
           ),
         ),
       ]),
