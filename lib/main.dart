2baaa0cb334068c3609a37f23cb412111f2ba9ef
FlutLab.io <>
2022-06-25 03:50:11 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/screens/signin_screen.dart';
 import 'package:myapp/screens/home_screen.dart';
-import 'package:myapp/screens/news_screen.dart';
 
 void main() async {
   runApp(MyApp());
