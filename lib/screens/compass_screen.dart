709232dd05b521b2fbe7063056826f4df5a5d9da
FlutLab.io <>
2022-07-12 03:29:15 +0000

    Autosave

---
+++ lib/screens/compass_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_compass/flutter_compass.dart';
+import 'dart:math';
+
+class HomeScreen extends StatefulWidget {
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  double? heading = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    FlutterCompass.events!.listen((event) {
+      setState(() {
+        heading = event.heading;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.grey.shade900,
+        centerTitle: true,
+        title: Text("Compass"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            "${heading!.ceil()}°",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 26.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(18.0),
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Image.asset("assets/cadrant.png"),
+                Transform.rotate(
+                  angle: ((heading ?? 0) * (pi / 180) * -1),
+                  child: Image.asset("assets/compass.png", scale: 1.1),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
